53a73f64334928dc2895435efd64e397ecb408c9
maxt0d0nt <maxbox21@hotmail.com>
2022-09-16 20:05:44 +0200

    delete task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux' /* asi se trae los datos*/
-
+import { useDispatch, useSelector } from 'react-redux' /* asi se trae los datos*/
+import { deleteTask } from '../features/task/taskSlice';
 
 export const TaskList = () => {
 
     const tasks = useSelector((state => state.tasks))
     console.log(tasks)
+
+    const dispatch = useDispatch()
+
+    const handleDelete = (id) => {
+        dispatch(deleteTask(id))
+    }
 
     return (
         <>
@@ -13,6 +19,7 @@ export const TaskList = () => {
                 <div key={task.id}>
                     <h3>{task.title}</h3>
                     <p>{task.description}</p>
+                    <button onClick={() => handleDelete(task.id)}>Delete</button>
                 </div>
             ))}
         </>
--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -23,9 +23,16 @@ export const taskSlice = createSlice({
         addTask: (state, action) => {
             console.log(state, action)
             state.push(action.payload)
-        }
+        },
+
+        deleteTask: (state, action) => {
+            const taskFound = state.find(task => task.id === action.payload)
+            if (taskFound) {
+                state.splice(state.indexOf(taskFound), 1)
+            }
     }
+}
 })
 
-export const { addTask } = taskSlice.actions
+export const { addTask, deleteTask } = taskSlice.actions
 export default taskSlice.reducer
